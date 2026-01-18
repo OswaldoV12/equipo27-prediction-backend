0e2f27e2ffd72796af5b3d4fa09874690b4098eb
brendayw <brendayw97@gmail.com>
2026-01-18 01:38:23 -0300

    corrección para que cors no bloquee rutas públicas

--- src/main/java/com/h12_25_l/equipo27/backend/config/SecurityConfig.java
+++ src/main/java/com/h12_25_l/equipo27/backend/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.h12_25_l.equipo27.backend.seguridad.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -20,6 +19,8 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf
                         .ignoringRequestMatchers("/h2-console/**")
+                        .ignoringRequestMatchers("/api/**")
+                        .ignoringRequestMatchers("/auth/**")
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
