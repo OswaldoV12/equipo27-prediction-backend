bbc44a60c0cd881a0853530dd69ea2b2c3af70fa
JoseOswaldo <oswaback12@gmail.com>
2026-01-19 19:24:09 -0600

    Migracion completado;se volvio aplicar seguridad a DashboardController

--- src/main/java/com/h12_25_l/equipo27/backend/controller/DashboardController.java
+++ src/main/java/com/h12_25_l/equipo27/backend/controller/DashboardController.java
@@ -3,6 +3,7 @@ package com.h12_25_l.equipo27.backend.controller;
 import com.h12_25_l.equipo27.backend.dto.dashboard.DashboardSummaryDTO;
 import com.h12_25_l.equipo27.backend.dto.dashboard.PredictionHistoryDTO;
 import com.h12_25_l.equipo27.backend.service.dashboard.DashboardService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,18 +22,21 @@ public class DashboardController {
     }
 
     // Endpoint para resumen de predicciones
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/summary")
     public DashboardSummaryDTO getSummary() {
         return dashboardService.getSummary();
     }
 
     // Endpoint para historial temporal por vuelo
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     @GetMapping("/history")
     public List<PredictionHistoryDTO> getHistory(@RequestParam Long vueloId) {
         return dashboardService.getHistory(vueloId);
     }
 
     // Historial global de todos los vuelos
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/global-history")
     public List<PredictionHistoryDTO> getGlobalHistory() {
         return dashboardService.getGlobalHistory();
